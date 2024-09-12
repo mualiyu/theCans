11d2a1bd02ca1319341257bf739176e929dd770f
Muktar Usman <mualiyuoox@gmail.com>
2024-09-12 11:12:57 +0100

    Emails

--- config/mail.php
+++ config/mail.php
@@ -48,8 +48,8 @@ return [
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
 
-            'auth_mode' => null,
-            'verify_peer' => false,
+            // 'auth_mode' => null,
+            // 'verify_peer' => false,
         ],
 
         'ses' => [
